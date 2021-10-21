4d12e536fbb3566b5c0b4cdde5909e5c99725a11
Aarush Goradia <80135238+Super45coder@users.noreply.github.com>
2021-10-20 19:05:47 -0600

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,8 @@ pickaxe = "Rock";
 coal = 0;
 function money(){
 if (pickaxe == "Rock"){
-setInterval(1);
+setInterval(1000);
+clearInterval();
 coal = coal + 1;
 document.getElementById("coal").innerHTML = coal;
 money();
